612b3f8d707fdbcb477dea2472708a8847f89751
sbrink00 <sbrink00@stuy.edu>
2019-04-29 09:57:28 -0400

    using driver to print out heap

---
+++ driver.java
@@ -0,0 +1,7 @@
+public class driver{
+  public static void main(String[]args){
+    int[] ary = {5, 4, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+    String heap = HeapPrinter.toString(ary);
+    System.out.println(heap);
+  }
+}
