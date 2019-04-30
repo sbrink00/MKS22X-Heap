82343ed606ded2879bb1ead7ebb49be207ccfea1
sbrink00 <sbrink00@stuy.edu>
2019-04-29 21:40:29 -0400

    wrote heapsort using private methods

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,49 @@
+
 public class MyHeap{
-  //parent is n-1/2
-  //child is 2n+1, 2n+2
-  private static void pushDown(int[] data, int size, int n){
-    if (2*n + 1 <= size) 
+  public static boolean hasLeft(int size, int idx){return 2*idx + 1 <= size;}
+  public static boolean hasRight(int size, int idx){return 2*idx + 2 <= size;}
+  public static int getLeft(int size, int idx){return 2*idx + 1;}
+  public static int getRight(int size, int idx){return 2*idx + 2;}
+
+  public static void swap(int[] ary, int idx1, int idx2){
+    int temp = ary[idx2];
+    ary[idx2] = ary[idx1];
+    ary[idx1] = temp;
+  }
+
+  public static void pushDown(int[] data, int size, int idx){
+    if (!hasLeft(size, idx)) return;
+    else if (!hasRight(size, idx)){
+      if ((data[getLeft(size, idx)] > data[idx])) swap(data, idx, getLeft(size, idx));
+      return;
+    }
+    else{
+      int leftIdx = getLeft(size, idx);
+      int rightIdx = getRight(size, idx);
+      int left = data[leftIdx];
+      int right = data[rightIdx];
+      int maxChild = Math.max(left, right);
+      if (data[idx] >= maxChild) return;
+      if (maxChild == left){
+        swap(data, idx, leftIdx);
+        pushDown(data, size, leftIdx);
+      }
+      else swap(data, idx, rightIdx);
+      pushDown(data, size, rightIdx);
+    }
+  }
+
+  public static void heapify(int[] data){
+    for (int idx = data.length - 1; idx >= 0; idx --) pushDown(data, data.length - 1, idx);
+  }
+
+  public static void heapSort(int data[]){
+    heapify(data);
+    int size = data.length - 1;
+    while (size != 0){
+      swap(data, 0, size);
+      size --;
+      pushDown(data, size, 0);
+    }
   }
 }
